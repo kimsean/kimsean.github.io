b4f581ea6443d42f97acfd0d493260e93c700ae8
Kim Sean <kshonne.developer@gmail.com>
2021-04-07 12:55:40 +0800

    Updates

--- precache-manifest.899c1bb85fbd57930f775e568b3aae9d.js
+++ precache-manifest.899c1bb85fbd57930f775e568b3aae9d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "acd79ccfc8a2e1bf7bf1d8908281032b",
+    "revision": "9fb8865d047dc803b7617bbe600817c6",
     "url": "/kimsean.github.io/index.html"
   },
   {
-    "revision": "0a3c03d8699ab77fb3d1",
+    "revision": "1b2c8fbae13a67b5854d",
     "url": "/kimsean.github.io/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "2bd935d1fcd403360cc6",
+    "revision": "fa8b82acec8ea27b95dd",
     "url": "/kimsean.github.io/static/css/main.1e7a30ca.chunk.css"
   },
   {
-    "revision": "0a3c03d8699ab77fb3d1",
-    "url": "/kimsean.github.io/static/js/2.630e0d40.chunk.js"
+    "revision": "1b2c8fbae13a67b5854d",
+    "url": "/kimsean.github.io/static/js/2.f80e7f05.chunk.js"
   },
   {
     "revision": "c13766e7ab00f3713c0e15e008f04703",
-    "url": "/kimsean.github.io/static/js/2.630e0d40.chunk.js.LICENSE"
+    "url": "/kimsean.github.io/static/js/2.f80e7f05.chunk.js.LICENSE"
   },
   {
-    "revision": "2bd935d1fcd403360cc6",
-    "url": "/kimsean.github.io/static/js/main.0790cc29.chunk.js"
+    "revision": "fa8b82acec8ea27b95dd",
+    "url": "/kimsean.github.io/static/js/main.b031d63c.chunk.js"
   },
   {
     "revision": "bfbd7e9fccdd0b4b3ba2",
